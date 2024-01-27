f5ba52762cd22c3dc55dfff60578bdcad8e099cd
aym-n <ajm8982@gmail.com>
2024-01-27 21:54:58 +0530

    📝 Add: Instruction lines

--- src/chunk.rs
+++ src/chunk.rs
@@ -7,6 +7,7 @@ pub enum OpCode {
 
 pub struct Chunk {
     pub code: Vec<u8>,
+    lines: Vec<usize>,
     constants: ValueArray,
 }
 
@@ -14,16 +15,19 @@ impl Chunk {
     pub fn new() -> Self {
         Self {
             code: Vec::new(),
+            lines: Vec::new(),
             constants: ValueArray::new(),
         }
     }
 
-    pub fn write(&mut self, byte: u8) {
+    pub fn write(&mut self, byte: u8, line: usize) {
         self.code.push(byte);
+        self.lines.push(line);
     }
 
-    pub fn write_opcode(&mut self, opcode: OpCode) {
-        self.write(opcode as u8);
+    pub fn write_opcode(&mut self, opcode: OpCode, line: usize) {
+        self.code.push(opcode as u8);
+        self.lines.push(line);
     }
 
     pub fn free(&mut self) {
@@ -48,6 +52,12 @@ impl Chunk {
 
     fn disassemble_instruction(&self, offset: usize) -> usize {
         print!("{:04} ", offset);
+
+        if offset > 0 && self.lines[offset] == self.lines[offset - 1]{
+            print!("   | ");
+        } else {
+            print!("{:4} ", self.lines[offset]);
+        }
 
         let instruction = self.code[offset];
         match instruction {
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ fn main() {
     let mut chunk = Chunk::new();
 
     let constant = chunk.add_constant(1.2);
-    chunk.write_opcode(OpCode::OpConstant);
-    chunk.write(constant);
+    chunk.write_opcode(OpCode::OpConstant, 123);
+    chunk.write(constant, 123);
 
-    chunk.write(OpCode::OpReturn as u8);
+    chunk.write(OpCode::OpReturn as u8, 123);
     chunk.disassemble("test chunk");
     chunk.free();
 }
