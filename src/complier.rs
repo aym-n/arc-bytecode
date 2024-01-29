45d5de8930538a44bdf0ce271bf1d1576e3a0f94
aym-n <ajm8982@gmail.com>
2024-01-29 10:58:50 +0530

    🎱 Add: Tokens

--- src/complier.rs
+++ src/complier.rs
@@ -2,4 +2,19 @@ use crate::scanner;
 
 pub fn compile(source: String) {
     let mut scanner = scanner::Scanner::new(source);
+    loop {
+        let token = scan_token();
+        if(token.line != scanner.line) {
+            print!("{}", token.line);
+            scanner.line = token.line;
+        }else{
+            print!("   |");
+        }
+
+        print!("{%2} '{%.*s} \n'", token.token_type, token.length, token.start);
+
+        if token.token_type == TokenType::EOF {
+            break;
+        }
+    }
 }
--- src/scanner.rs
+++ src/scanner.rs
@@ -14,4 +14,66 @@ impl Scanner {
             line: 1,
         }
     }
+
+    pub fn scan_token(&mut self) -> Token{
+        self.start = self.current;
+        
+        if self.is_at_end() {
+            return Token::new(TokenType::EOF, self);
+        }
+
+        return Token::error_token("Unexpected character.", self);
+    }
+
+    fn is_at_end(&self) -> bool {
+        self.source.chars().nth(self.current) == Some('\0')
+    }
+}
+
+pub struct Token {
+    token_type: TokenType,
+    start: usize,
+    length: usize,
+    line: usize,
+}
+
+pub enum TokenType {
+    LeftParen, RightParen,
+    LeftBrace, RightBrace,
+    Comma, Dot, Minus, Plus,
+    Semicolon, Slash, Star,
+
+    Bang, BangEqual,
+    Equal, EqualEqual,
+    Greater, GreaterEqual,
+    Less, LessEqual,
+
+    Identifier, String, Number,
+
+    And, Class, Else, False,
+    Fn, For, If, Nil, Or,
+    Print, Return, Super, This,
+    True, Var, While,
+
+    Error, EOF,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, scanner:&mut Scanner) -> Self {
+        Self {
+            token_type,
+            start: scanner.start,
+            length: (scanner.current - scanner.start),
+            line: scanner.line,
+        }
+    }
+
+    pub fn error_token(message: &str, scanner: &mut Scanner) -> Self {
+        Self {
+            token_type: TokenType::Error,
+            start: 0,
+            length: message.len(),
+            line: scanner.line,
+        }
+    }
 }
